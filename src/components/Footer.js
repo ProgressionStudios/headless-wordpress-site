9c800fc9f8a768afdc549001ce274c1232d7cee0
Mike Garcia <mike@progressionstudios.com>
2024-04-05 22:10:14 -0700

    Initial Footer responsive styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,4 @@
+import SubscribePage from '../lib/newslettersignup';
 import Link from 'next/link';
 
 import styles from '../styles/layout/Footer.module.css';
@@ -13,7 +14,7 @@ function Footer() {
                             <h4>Get notified when new themes are released!</h4>
                         </div>
                         <div className={styles.newsletterForm}>
-                            MailChimp Component
+                            Newsletter goes here SubscribePage
                         </div>
                     </div>
                 </div>
@@ -22,12 +23,14 @@ function Footer() {
                 <div className={styles.footerMain}>
                     <h2 className={styles.title}>Let us build your website.</h2>
                     <h3 className={styles.description}>We can help create and update your website. Tell us about your next project.</h3>
-                    <Link href="/contact-us" className={styles.button}>
-                        <button>
-                            Hire us today!
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><title id="Email-Icon">Email</title><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" /></svg>
-                        </button>
-                    </Link>
+                    <div className={styles.footerBtn}>
+                        <div className="alt-btn">
+                            <a href="/contat-us" className="button">
+                                Hire us today!
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><title id="Email-Icon">Email</title><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" /></svg>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className={styles.copyright}>
@@ -36,7 +39,7 @@ function Footer() {
                     <span className={styles.divider}></span>
                     All Rights Reserved.
                     <span className={styles.divider}></span>
-                    Developed by <Link href="/contact-us">Progression Studios</Link>.
+                    Developed by <Link href="/contact-us" className={styles.footerlink} >Progression Studios</Link>.
                 </div>
             </div>
         </footer>
---
+++ src/lib/newslettersignup.js
@@ -0,0 +1,41 @@
+export default function SubscribePage() {
+    return (
+        <div id="mc_embed_shell">
+            <div id="mc_embed_signup">
+                <form action="https://progressionstudios.us1.list-manage.com/subscribe/post?u=1a06aa3bca8613232881e8a6e&amp;id=2f5a556941&amp;f_id=0068f9e5f0"
+                    method="post"
+                    id="mc-embedded-subscribe-form"
+                    name="mc-embedded-subscribe-form"
+                    className="validate"
+                    target="_self"
+                    noValidate>
+                    <div id="mc_embed_signup_scroll">
+                        <div className="mc-field-group">
+                            <label htmlFor="mce-EMAIL">Email Address <span className="asterisk">*</span></label>
+                            <input type="email" name="EMAIL" className="required email" id="mce-EMAIL" required value="" />
+                        </div>
+                        <div id="mce-responses" className="clear foot">
+                            <div className="response" id="mce-error-response" style={{ display: 'none' }}></div>
+                            <div className="response" id="mce-success-response" style={{ display: 'none' }}></div>
+                        </div>
+                        <div aria-hidden="true" style={{ position: 'absolute', left: '-5000px' }}>
+                            <input type="text" name="b_1a06aa3bca8613232881e8a6e_2f5a556941" tabIndex="-1" value="" />
+                        </div>
+                        <div className="optionalParent">
+                            <div className="clear foot">
+                                <input type="submit" name="subscribe" id="mc-embedded-subscribe" className="button" value="Subscribe" />
+                                <p style={{ margin: '0px auto' }}>
+                                    <a href="http://eepurl.com/iNevPo" title="Mailchimp - email marketing made easy and fun">
+                                        <span style={{ display: 'inline-block', backgroundColor: 'transparent', borderRadius: '4px' }}>
+                                            <img className="refferal_badge" src="https://digitalasset.intuit.com/render/content/dam/intuit/mc-fe/en_us/images/intuit-mc-rewards-text-dark.svg" alt="Intuit Mailchimp" style={{ width: '220px', height: '40px', display: 'flex', padding: '2px 0', justifyContent: 'center', alignItems: 'center' }} />
+                                        </span>
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
