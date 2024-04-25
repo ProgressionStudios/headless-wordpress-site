f64392b5fe3fe160c31f50f575c3a1fd3d658e89
Mike Garcia <mike@progressionstudios.com>
2024-04-25 15:49:47 -0700

    Homepage content update including new About section

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import parse from 'html-react-parser';
 import seoStringParser from "../lib/utilities/seoStringParser";
 import Link from 'next/link';
 import { getThemesList } from "../lib/themes";
-import OurServices from "../lib/home/OurServices";
 import ThemeList from "../components/ThemeList";
 
 import styles from "../styles/content/page.module.css";
@@ -17,7 +16,7 @@ export default async function Home() {
         <div className="container">
           <div className={styles.heroText}>
             <h1 className={styles.heroTitle}>WordPress Experts</h1>
-            <h2 className={styles.heroSubtitle}>Specializing in developing WordPress Themes on ThemeForest.net</h2>
+            <h2 className={styles.heroSubtitle}>We build Premium WordPress Themes available on ThemeForest.net</h2>
             <div className="alt-btn xl-btn"><Link href="/portfolio" className="button">Explore Themes</Link></div>
           </div>
         </div>
@@ -28,18 +27,25 @@ export default async function Home() {
             src="/images/mobile-hero.webp"
             alt="WordPress Experts"
             decoding="async"
-            loading="lazy"
             width="800"
             height="500"
             rel="preload" 
-            fetchPriority="high"
             type="image/webp"
             as="image"
             className={styles.heroMobile}
           />
         <div className={styles.heroOverlay}></div>
       </div>
-      <OurServices />
+      <div className="container">
+        <div className="heading-section-global">
+          <h2>Recent Themes</h2>
+          <h3>Check out the latest WordPress Themes created by us.</h3>
+        </div>
+        <ThemeList initialPosts={initialPosts} count={3} />
+        <div className="button-center-grid">
+          <Link href="/portfolio" className="button">More Themes</Link>
+        </div>
+      </div>
       <div className={styles.eliteSection}>
         <div className="container">
           <div className={styles.eliteGrid}>
@@ -53,19 +59,39 @@ export default async function Home() {
             />
             <div>
               <h2>$2+ Million in Sales on ThemeForest</h2>
-              <h3>Join over <strong>50,000</strong> happy customers that are already using our services!</h3>
+              <h3>Join over <strong>60,000</strong> happy customers that are already using our services!</h3>
             </div>
           </div>
         </div>
       </div>
       <div className="container">
-        <div className="heading-section-global">
-          <h2>WordPress Themes</h2>
-          <h3>Check out the latest WordPress Themes created by us.</h3>
-        </div>
-        <ThemeList initialPosts={initialPosts} count={3} />
-        <div className="button-center-grid">
-          <Link href="/portfolio" className="button">More Themes</Link>
+        <div className={styles.aboutContainer}>
+
+          <div className={styles.aboutText}>
+              <h2>Meet Michael Garcia</h2>
+              <h3>About Progression Studios</h3>
+              <div className={styles.aboutparagraph}>
+              <p>I am Michael Garcia, the creator and owner of Progression Studios. My passion lies in working with WordPress, where I specialize in developing Premium WordPress Themes. </p>
+                
+              <p>Over the last 13 years, I have dedicated myself to creating themes and plugins for sale on ThemeForest.net. During that time I have attained the status of &quot;Power Elite Author&quot; on ThemeForest with more than 60,000 WordPress Themes sold.</p>
+
+              </div>
+          </div>
+
+          <div className={styles.aboutImage}>
+            <img
+              src="/images/michael.webp"
+              alt="Michael Garcia"
+              decoding="async"
+              loading="lazy"
+              width="800"
+              height="743"
+              rel="preload"
+              type="image/webp"
+              as="image"
+            />
+          </div>
+
         </div>
       </div>
     </main>
